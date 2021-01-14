cd7ad5bae7eea7188dd00c896f5d0b23987372c4
Navya Ramagiri <navyaramagiri@gmail.com>
2021-01-13 21:28:56 -0500

    deleted test files

--- client/components/ToyList.spec.js
+++
@@ -1,8 +0,0 @@
-import {expect} from 'chai'
-import React from 'react'
-import enzyme, {shallow} from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-import {ToyList} from './ToyList'
-
-const adapter = new Adapter()
-enzyme.configure({adapter})
